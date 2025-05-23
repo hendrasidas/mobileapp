4f3cb7c858aa2674a0b7e3763ea4fcb7f41b41c0
hendrasidas <60605587+hendrasidas@users.noreply.github.com>
2025-05-23 17:09:54 +0700

    Update loginapp.py

--- loginapp.py
+++ loginapp.py
@@ -4,6 +4,7 @@ from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.popup import Popup
+import streamlit as st
 
 
 class LoginScreen(BoxLayout):
